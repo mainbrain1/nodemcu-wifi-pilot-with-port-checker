bcaa40465f0db88ed3458f8f478db4619b084463
mainbrain1 <taz.govna@gmail.com>
2017-01-26 11:27:38 +0300

    Add files via upload

--- query2.lua
+++ query2.lua
@@ -1,11 +1,12 @@
 function startup2 ()
 for s=1 , 4 do
 print (test2[s])
-	if (test2[s]==1)then
-               if (test[s]==1)then
-                    gpio.write(led[s], gpio.HIGH);
-               elseif(test[s]==0)then
-                    gpio.write(led[s], gpio.LOW);
+local x=tostring(s)
+	if (test2[x]==1)then
+               if (test[x]==1)then
+                    gpio.write(led[x], gpio.HIGH);
+               elseif(test[x]==0)then
+                    gpio.write(led[x], gpio.LOW);
                end
         end
 end
